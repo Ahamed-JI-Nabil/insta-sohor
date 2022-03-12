f75122c9b839b3ba5d4eb6ca4318cec1ede2865a
AhamedJINabil <johayeriqbal@gmail.com>
2022-03-12 21:25:59 +0600

    like post fixed!

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let posts=[ ];
 
-const likedPostsId = [];
+let likedPostsId = [];
 const reportedPostsId = [];
 
 const getLikedPosts = () => {
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+    likedPostsId+=(id); 
     showPosts(posts);
 };
 
